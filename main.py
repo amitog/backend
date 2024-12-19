4f095b291c6dab8e15031501f5e362faade1aa90
amitog <write2amitog@gmail.com>
2024-12-19 17:49:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,4 +49,5 @@ def upload_image():
         return "File type not allowed."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
